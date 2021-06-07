bd9e1409dd9b865ea6846cd154912f17a4e94b6f
Cali Castle <cali@calicastle.com>
2021-06-07 23:36:33 +0800

    Fix SSR

--- pages/index.vue
+++ pages/index.vue
@@ -3,30 +3,25 @@
     class="min-h-screen flex flex-col justify-center items-center bg-gray-800"
   >
     <h1 class="text-2xl text-gray-200">SSR Demo</h1>
-    <template v-if="users.length > 0">
-      <h2 class="font-bold mt-4 text-blue-400">Users:</h2>
-      <ul class="mt-3 flex flex-col space-y-2">
-        <li
-          v-for="user in users"
-          :key="user.name"
-          class="text-green-500 font-medium text-lg"
-        >
-          {{ user.name }}
-        </li>
-      </ul>
-    </template>
+    <h2 class="font-bold mt-4 text-blue-400">{{ message }}</h2>
+    <ul class="mt-3 flex flex-col space-y-2">
+      <li
+        v-for="user in users"
+        :key="user.name"
+        class="text-green-500 font-medium text-lg"
+      >
+        {{ user.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
-  ref,
-  ssrPromise,
+  onServerPrefetch,
   ssrRef,
   useContext,
-  useFetch,
-  useMeta,
 } from '@nuxtjs/composition-api'
 
 type UserDto = {
@@ -44,19 +39,16 @@ export default defineComponent({
     const message = ssrRef('')
     const users = ssrRef<UserDto[]>([])
 
-    const { title } = useMeta()
     const { $http } = useContext()
 
-    ssrPromise(async () => {
+    onServerPrefetch(async () => {
       const data = await $http.$get<TestApiResponse>('/api/test')
 
       message.value = data.message
       users.value = data.users
-
-      title.value = `${message.value} with ${users.value.length} users`
     })
 
-    return { users }
+    return { message, users }
   },
 
   head: {},
